cdea6192cbf45fba7252b0752c67c2b402c45335
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-02-21 18:33:47 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='morar',
+	version='0.1'
+	url='http://github.com/swarchal/morar',
+	description='Data management for CellProfiler',
+	author='Scott Warchal',
+	author_email='s.warchal@sms.ed.ac.uk',
+	license='MIT',
+	packages=['morar'],
+	test_suite='nose.collector',
+	test_require=['nose'],
+	install_requires=['sqlalchemy', 'pandas']
+	zip_safe=False)
+
