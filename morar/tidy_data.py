1fbbc4571eb393105ce59be545bded14c76b8300
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-14 11:51:16 +0000

    define feature/meta-data at the start

--- morar/tidy_data.py
+++ morar/tidy_data.py
@@ -24,7 +24,9 @@ class tidy_data:
     """
 
 
-    def __init__(self, path, storage = "csv"):
+    def __init__(self, path, storage = "csv", metadata_prefix = "Metadata", plate = "Plate", well = "Well", sep = "_"):
+        
+        # load data from csv or sqlite database
         try:
             if storage == "csv":
                 self.data = pd.read_csv(path)
@@ -34,61 +36,40 @@ class tidy_data:
         except ValueError:
             print "%s is not a valid format, try either 'csv' or 'sqlite'" % storage
         assert isinstance(self.data, pd.DataFrame)
-
-
-    
-    def get_metadata(self, metadata_prefix = "Metadata"):
-        """
-        Returns list of columns that correspond 
-        to metadata.
-        """
+        
+        # get and define metadata columns
         self.metadata_prefix = metadata_prefix
         self.metadata_cols = []
         for col in self.data.columns:
             if self.metadata_prefix in col:
                 self.metadata_cols.append(col)
 
-
-    # TODO: how to handle site repeats? 
-    def define_metadata(self, plate = "Plate", well = "Well", sep = "_"):
-	"""
-	Defines specific metadata columns needed for aggregating
-	and normalising functions
-	- Plate ID
-	- Well ID
-
-	The sep argument is the character used to separate the plate/well string
-	from the metadata_prefix that has already been defined in 'get_metadata'
-	e.g Metadata_Plate, Metadata_Well
-	"""
-
-	# TODO check metadata has already been defined
-
         self.plate_col = self.metadata_prefix + sep + plate
         self.well_col = self.metadata_prefix + sep + well
 
         if self.plate_col not in self.data.columns:
-            raise ValueError('%s was not found in available columns' % self.plate_col)
+            raise ValueError("%s not found in columns" % self.plate_col)
         if self.well_col not in self.data.columns:
-            raise ValueError('%s was not found in available columns' %self.well_col)
+            raise ValueError("%s not found in columns" % self.well_col)
+  
+        # get featuredata columns
+        self.feature_data_cols = list(set(self.data.columns) - set(self.metadata_cols))
+        assert len(list(self.feature_data_cols)) >= 1
 
 
-    def get_featuredata(self, numeric_only = True):
+
+    def get_numeric_featuredata(self, numeric_only = True):
         """
         Returns list of columns that correspond
         to feature data. i.e not metadata
         """
-	# get columns that are not metadata
-        self.featuredata_cols = list(set(self.data.columns) - set(self.metadata_cols))
+	# of those columns, get only the numeric columns
+        tmp = self.data[self.featuredata_cols].select_dtypes(include=[np.number])
+        self.featuredata_cols = tmp.columns
 	# check there is at least one column
 	assert len(list(self.featuredata_cols)) >= 1
-        if numeric_only:
-	    # of those columns, get only the numeric columns
-            tmp = self.data[self.featuredata_cols].select_dtypes(include=[np.number])
-            self.featuredata_cols = tmp.columns
-	    # check there is at least one column
-	    assert len(list(self.featuredata_cols)) >= 1
         return list(self.featuredata_cols)
+
 
 
     def get_no_variance_col(self, tolerance = 1e-5):
@@ -159,10 +140,6 @@ class tidy_data:
 if __name__ == "__main__":
 
     test = tidy_data('/home/scott/Dropbox/Public/df_cell_subclass.csv')
-
-    test.get_metadata("Metadata")
-    test.get_featuredata()
-    test.define_metadata()
     print test.plate_col
     print test.well_col
     print test.aggregate_well()
