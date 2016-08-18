060eeecaad40071cb9a22cc33e95846aa2fac2aa
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-08-18 19:20:53 +0100

    normalise now a module rather than a class

--- morar/__init__.py
+++ morar/__init__.py
@@ -1,2 +1,2 @@
 import stats
-from normalise import Normalise
+import normalise
--- morar/normalise.py
+++ morar/normalise.py
@@ -1,94 +1,117 @@
-import stats
+from morar import stats
 import pandas as pd
 import numpy as np
 
 
-class Normalise():
+def get_featuredata(df, metadata_prefix="Metadata"):
     """
-    Normalisation functions for pandas dataframes.
+    identifies columns in a dataframe that are not labelled with the
+    metadata prefix. Its assumed everything not labelled metadata is
+    featuredata
+    -----------------------------------------------------------------------
+    df: dataframe
+    metadata_prefix: string, prefix for metadata columns
     """
+    f_cols = [i for i in df.columns if not i.startswith(metadata_prefix)]
+    return f_cols
 
 
-    def __init__(self, data):
-        if not isinstance(data, pd.DataFrame):
-            raise ValueError("input is not a pandas DataFrame")
-        self.df = data
+def check_control(df, plate_id, compound="Metadata_compound",
+                  neg_compound="DMSO"):
+    """
+    check each plate contains at least 1 negative control value
+    -------------------------------------------------------------------------
+    df: dataframe
+    plate_id: string for column containing plate ID/label
+    compound: string for column containing compound name/ID
+    neg_compound: string name of negative control compound in compound col
+    """
+    for name, group in df.groupby(plate_id):
+        group_cmps = list(set(group[compound]))
+        if neg_compound not in group_cmps:
+            raise RuntimeError("{} does not contain any negative control values".format(name))
 
 
-    def get_featuredata(self, df, metadata_prefix="Metadata"):
-        """
-        identifies columns in a dataframe that are not labelled with the
-        metadata prefix. Its assumed everything not labelled metadata is
-        featuredata
-        -----------------------------------------------------------------------
-        metadata_prefix: string, prefix for metadata columns
-        """
-        f_cols = [i for i in self.df.columns if not i.startswith(metadata_prefix)]
-        return f_cols
+def normalise(df, plate_id, compound="Metadata_compound",
+              neg_compound="DMSO", method="divide",
+              metadata_prefix="Metadata"):
+    """
+    Normalise values against DMSO values per plate.
+    -----------------------------------------------------------------------
+    df: dataframe
+    plate_id: string for column containing plate ID/label
+    compound: string for column containing compound name/ID
+    neg_compound: string name of negative control compound in compound col
+    method: method to normalise against negative control
+    metadata_prefix: string, prefix for metadata columns
+    """
+    valid_methods = ["subtract", "divide"]
+    if method not in valid_methods:
+        raise ValueError("Invalid method, options: 'subtract', 'divide'")
+    # check there are some negative controls on each plate
+    check_control(df, plate_id, compound, neg_compound)
+    # identify feature columns
+    f_cols = get_featuredata(df, metadata_prefix)
+    df_out = pd.DataFrame()
+    # group by plate
+    grouped = df.groupby(plate_id, as_index=False)
+    # calculate the average DMSO values for each plate
+    for _, group in grouped:
+        # TODO keep metadata columns
+        dmso_med = list(group[group[compound] == neg_compound][f_cols].median().values)
+        assert len(dmso_med) == group[f_cols].shape[1]
+        if method == "subtract":
+            # TODO fix subtraction calculation
+            tmp = group[f_cols].sub(dmso_med)
+        if method == "divide":
+            tmp = group[f_cols].divide(dmso_med)
+        # concatenate group to overall dataframe
+        df_out = pd.concat([df_out, tmp])
+    # check we have not lost any rows
+    assert df.shape[1] == df_out.shape[1]
+    return df_out
 
 
-    def check_control(self, df, plate_id, compound="Metadata_compound",
-                      neg_compound="DMSO"):
-        """ check each plate contains at least one negative control """
-        pass
+def robust_normalise(df, plate_id, compound="Metadata_compound",
+                     neg_compound="DMSO", metadata_prefix="Metadata"):
+    """
+    Method used in the Carpenter lab. Substract the median feature value for
+    each plate DMSO from the treatment feature value and divide by the
+    median absolute deviation.
+    Returns a pandas DataFrame.
+    -----------------------------------------------------------------------
+    df: dataframe
+    plate_id: string for column containing plate ID/label
+    compound: string for column containing compound name/ID
+    neg_compound: string name of negative control compound in compound col
+    metadata_prefix: string, prefix for metadata columns
+    """
+    check_control(df, plate_id, compound, neg_compound)
+    f_cols = get_featuredata(df, metadata_prefix)
+    grouped = df.groupby(plate_id, as_index=False)
+    df_out = pd.DataFrame()
+    # calculate the average DMSO values per plate_id
+    for _, group in grouped:
+        # find the median and mad dmso value for each plate
+        dmso_vals = group[group[compound] == neg_compound]
+        dmso_med = dmso_vals[f_cols].median().values.tolist()
+        dmso_mad = dmso_vals[f_cols].apply(lambda x: stats.mad(x), axis=0).values.tolist()
+        assert len(dmso_med) == group[f_cols].shape[1]
+        # subtract each row of the group by that group's DMSO values
+        tmp = group[f_cols].sub(dmso_med, axis=1)
+        # divide by the MAD of the negative control
+        tmp = tmp.apply(lambda x: (x/dmso_mad)*1.4826, axis=1)
+        # concatenate group to overall dataframe
+        df_out = pd.concat([df_out, tmp])
+    # check we have not lost any rows
+    assert df.shape[1] == df_out.shape[1]
+    return df_out
 
 
-    def normalise(self, plate_id, compound="Metadata_compound",
-                  neg_compound="DMSO", method="divide",
-                  metadata_prefix="Metadata"):
-        """
-        Normalise values against DMSO values per plate.
-        -----------------------------------------------------------------------
-        plate_id: string for column containing plate ID/label
-        compound: string for column containing compound name/ID
-        neg_compound: string name of negative control compound in compound col
-        method: method to normalise against negative control
-        metadata_prefix: string, prefix for metadata columns
-        """
-        valid_methods = ["subtract", "divide"]
-        if method not in valid_methods:
-            raise ValueError("Invalid method, options: 'subtract', 'divide'")
-        # check there are some negative controls on each plate
-        self.check_control(self.df, plate_id, compound, neg_compound)
-        # identify feature columns
-        f_cols = self.get_featuredata(self.df, metadata_prefix)
-        df_out = pd.DataFrame()
-        # group by plate
-        grouped = self.df.groupby(plate_id, as_index=False)
-        # calculate the average DMSO values for each plate
-        for _, group in grouped:
-            # TODO keep metadata columns
-            dmso_vals = list(group[group[compound] == neg_compound][f_cols].median().values)
-            assert len(dmso_vals) == group[f_cols].shape[1]
-            if method == "subtract":
-                # TODO fix subtraction calculation
-                tmp = group[f_cols].apply(lambda x: x - dmso_vals)
-            if method == "divide":
-                tmp = group[f_cols].divide(dmso_vals)
-            df_out = pd.concat([df_out, tmp])
-        # check we have not lost any rows
-        assert self.df.shape[1] == df_out.shape[1]
-        return df_out
-
-
-    def robust_normalise(self, plate_id, compound="Metadata_compound",
-                         neg_compound="DMSO", metadata_prefix="Metadata"):
-        """
-        Method used in the Carpenter lab. Substract the median feature value for
-        each plate from the treatment feature value and divide by the median
-        absolute deviation.
-        Returns a pandas DataFrame.
-        -----------------------------------------------------------------------
-        plate_id: string for column containing plate ID/label
-        compound: string for column containing compound name/ID
-        neg_compound: string name of negative control compound in compound col
-        metadata_prefix: string, prefix for metadata columns
-        """
-        pass
-
-
-    def scale_features(self):
-        """
-        scale and centre features
-        """
-        pass
+def scale_features(df):
+    """
+    scale and centre features
+    --------------------------------------------------------------------------
+    df: dataframe
+    """
+    pass
