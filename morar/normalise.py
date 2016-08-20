57dbd2ab1c66230d0c2328217a49ec42326018a5
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-08-20 14:00:10 +0100

    working normalise, and get_metadata func

--- morar/normalise.py
+++ morar/normalise.py
@@ -8,28 +8,39 @@ def get_featuredata(df, metadata_prefix="Metadata"):
     identifies columns in a dataframe that are not labelled with the
     metadata prefix. Its assumed everything not labelled metadata is
     featuredata
-    -----------------------------------------------------------------------
-    df: dataframe
-    metadata_prefix: string, prefix for metadata columns
+
+    @param df dataframe
+    @param metadata_prefix string, prefix for metadata columns
     """
     f_cols = [i for i in df.columns if not i.startswith(metadata_prefix)]
     return f_cols
+
+
+def get_metadata(df, metadata_prefix="Metadata"):
+    """
+    identifies column in a dataframe that are labelled with the metadata_prefix
+
+    @param df pandas DataFrame
+    @param metadata_prefix metadata prefix in column name
+    """
+    m_cols = [i for i in df.columns if i.startswith(metadata_prefix)]
+    return m_cols
 
 
 def check_control(df, plate_id, compound="Metadata_compound",
                   neg_compound="DMSO"):
     """
     check each plate contains at least 1 negative control value
-    -------------------------------------------------------------------------
-    df: dataframe
-    plate_id: string for column containing plate ID/label
-    compound: string for column containing compound name/ID
-    neg_compound: string name of negative control compound in compound col
+
+    @param df dataframe
+    @param plate_id string for column containing plate ID/label
+    @param compound string for column containing compound name/ID
+    @param neg_compound string name of negative control compound in compound col
     """
     for name, group in df.groupby(plate_id):
         group_cmps = list(set(group[compound]))
         if neg_compound not in group_cmps:
-            raise RuntimeError("{} does not contain any negative control values".format(name))
+            raise ValueError("{} does not contain any negative control values".format(name))
 
 
 def normalise(df, plate_id, compound="Metadata_compound",
@@ -37,14 +48,15 @@ def normalise(df, plate_id, compound="Metadata_compound",
               metadata_prefix="Metadata"):
     """
     Normalise values against DMSO values per plate.
-    -----------------------------------------------------------------------
-    df: dataframe
-    plate_id: string for column containing plate ID/label
-    compound: string for column containing compound name/ID
-    neg_compound: string name of negative control compound in compound col
-    method: method to normalise against negative control
-    metadata_prefix: string, prefix for metadata columns
+
+    @param df dataframe
+    @param plate_id string for column containing plate ID/label
+    @param compound string for column containing compound name/ID
+    @param neg_compound string name of negative control compound in compound col
+    @param method method to normalise against negative control
+    @param metadata_prefix string, prefix for metadata columns
     """
+
     valid_methods = ["subtract", "divide"]
     if method not in valid_methods:
         raise ValueError("Invalid method, options: 'subtract', 'divide'")
@@ -52,23 +64,23 @@ def normalise(df, plate_id, compound="Metadata_compound",
     check_control(df, plate_id, compound, neg_compound)
     # identify feature columns
     f_cols = get_featuredata(df, metadata_prefix)
+    # dataframe for output
     df_out = pd.DataFrame()
     # group by plate
     grouped = df.groupby(plate_id, as_index=False)
     # calculate the average DMSO values for each plate
     for _, group in grouped:
         # TODO keep metadata columns
-        dmso_med = list(group[group[compound] == neg_compound][f_cols].median().values)
+        dmso_med = group[group[compound] == neg_compound][f_cols].median()
         assert len(dmso_med) == group[f_cols].shape[1]
         if method == "subtract":
-            # TODO fix subtraction calculation
             tmp = group[f_cols].sub(dmso_med)
         if method == "divide":
             tmp = group[f_cols].divide(dmso_med)
         # concatenate group to overall dataframe
         df_out = pd.concat([df_out, tmp])
     # check we have not lost any rows
-    assert df.shape[1] == df_out.shape[1]
+    assert df.shape[0] == df_out.shape[0]
     return df_out
 
 
@@ -79,12 +91,12 @@ def robust_normalise(df, plate_id, compound="Metadata_compound",
     each plate DMSO from the treatment feature value and divide by the
     median absolute deviation.
     Returns a pandas DataFrame.
-    -----------------------------------------------------------------------
-    df: dataframe
-    plate_id: string for column containing plate ID/label
-    compound: string for column containing compound name/ID
-    neg_compound: string name of negative control compound in compound col
-    metadata_prefix: string, prefix for metadata columns
+
+    @param df dataframe
+    @param plate_id string for column containing plate ID/label
+    @param compound string for column containing compound name/ID
+    @param neg_compound string name of negative control compound in compound col
+    @param metadata_prefix string, prefix for metadata columns
     """
     check_control(df, plate_id, compound, neg_compound)
     f_cols = get_featuredata(df, metadata_prefix)
@@ -94,24 +106,15 @@ def robust_normalise(df, plate_id, compound="Metadata_compound",
     for _, group in grouped:
         # find the median and mad dmso value for each plate
         dmso_vals = group[group[compound] == neg_compound]
-        dmso_med = dmso_vals[f_cols].median().values.tolist()
+        dmso_med = dmso_vals[f_cols].median().values
         dmso_mad = dmso_vals[f_cols].apply(lambda x: stats.mad(x), axis=0).values.tolist()
         assert len(dmso_med) == group[f_cols].shape[1]
         # subtract each row of the group by that group's DMSO values
-        tmp = group[f_cols].sub(dmso_med, axis=1)
+        tmp = group[f_cols].sub(dmso_med)
         # divide by the MAD of the negative control
         tmp = tmp.apply(lambda x: (x/dmso_mad)*1.4826, axis=1)
         # concatenate group to overall dataframe
         df_out = pd.concat([df_out, tmp])
     # check we have not lost any rows
-    assert df.shape[1] == df_out.shape[1]
+    assert df.shape[0] == df_out.shape[0]
     return df_out
-
-
-def scale_features(df):
-    """
-    scale and centre features
-    --------------------------------------------------------------------------
-    df: dataframe
-    """
-    pass
--- tests/test_normalise.py
+++ tests/test_normalise.py
@@ -25,7 +25,7 @@ def test_get_featuredata_middle_prefix():
     assert cols == ["colA", "colB", "something_Metadata"]
 
 
-def test_get_featuredata_middle_prefix():
+def test_get_featuredata_different_case():
     x = [1,2,3,4]
     y = [4,3,2,1]
     z = [1,2,3,4]
@@ -35,6 +35,37 @@ def test_get_featuredata_middle_prefix():
     cols = normalise.get_featuredata(test_df, metadata_prefix="metadata")
     assert cols == ["colA", "colB", "Metadata_A"]
 
+
+def test_get_metadata_simple():
+    x = [1,2,3,4]
+    y = [4,3,2,1]
+    z = [1,2,3,4]
+    columns = ["colA", "colB", "Metadata_A"]
+    test_df = pd.DataFrame(zip(x, y, z), columns=columns)
+    cols = normalise.get_metadata(test_df)
+    assert cols == ["Metadata_A"]
+
+
+def test_get_metadata_middle_prefix():
+    x = [1,2,3,4]
+    y = [4,3,2,1]
+    z = [1,2,3,4]
+    a = [4,3,5,1]
+    columns = ["colA", "colB", "something_Metadata", "Metadata_A"]
+    test_df = pd.DataFrame(zip(x, y, z, a), columns=columns)
+    cols = normalise.get_metadata(test_df)
+    assert cols == ["Metadata_A"]
+
+
+def test_get_metadata_different_case():
+    x = [1,2,3,4]
+    y = [4,3,2,1]
+    z = [1,2,3,4]
+    a = [4,3,5,1]
+    columns = ["colA", "colB", "metadata_A", "Metadata_A"]
+    test_df = pd.DataFrame(zip(x, y, z, a), columns=columns)
+    cols = normalise.get_metadata(test_df, metadata_prefix="metadata")
+    assert cols == ["metadata_A"]
 
 
 # create test DataFrame
@@ -68,52 +99,73 @@ missing_control_df = pd.DataFrame(zip(x, y, z, plate, compound), columns=colname
 
 
 
-@raises(RuntimeError)
+@raises(ValueError)
 def test_check_control():
     normalise.check_control(missing_control_df, plate_id="Metadata_plate")
-    # assert for RuntimeError
+    # assert for ValueError
 
 
-def test_normalise_returns_dataframe():
-    out = normalise.normalise(df, plate_id="Metadata_plate")
+@raises(ValueError)
+def test_check_control_within_function():
+    normalise.normalise(missing_control_df, plate_id="Metadata_plate")
+    # assert for ValueError
+
+
+def test_normalise_returns_dataframe_subtract():
+    out = normalise.normalise(df, plate_id="Metadata_plate", method="subtract")
+    assert isinstance(out, pd.DataFrame)
+
+
+def test_normalise_returns_dataframe_divide():
+    out = normalise.normalise(df, plate_id="Metadata_plate", method="divide")
     assert isinstance(out, pd.DataFrame)
 
 
 def test_normalise_returns_correct_size():
     out = normalise.normalise(df, plate_id="Metadata_plate")
-    assert out.shape[1] == df.shape[1]
+    assert out.shape[0] == df.shape[0]
 
 
 def test_normalise_returns_correct_values():
     out = normalise.normalise(df, plate_id="Metadata_plate")
-    pass
+    assert isinstance(out, pd.DataFrame)
+    assert out.shape[0] == df.shape[0]
+    # TODO assert correct values
 
 
 def test_normalise_method_subtract():
     out = normalise.normalise(df, plate_id="Metadata_plate")
-    pass
+    assert isinstance(out, pd.DataFrame)
+    # TODO assert correct values
 
 
 def test_normalise_method_divide():
     out = normalise.normalise(df, plate_id="Metadata_plate")
-    pass
+    assert isinstance(out, pd.DataFrame)
+    # TODO assert correct values
 
 
 def test_normalise_non_default_cols():
     out = normalise.normalise(non_default_df, metadata_prefix="meta",
                               compound="meta_cmpd", plate_id="meta_plate")
     assert isinstance(out, pd.DataFrame)
-    assert out.shape[1] == non_default_df.shape[1]
-
+    assert out.shape[0] == non_default_df.shape[0]
 
 
 def test_robust_normalise_returns_dataframe():
-    pass
+    out = normalise.robust_normalise(df, plate_id="Metadata_plate")
+    assert isinstance(out, pd.DataFrame)
 
 
 def test_robust_normalise_returns_correct_size():
-    pass
+    out = normalise.robust_normalise(df, plate_id="Metadata_plate")
+    assert isinstance(out, pd.DataFrame)
+    assert df.shape[0] == out.shape[0]
+    # TODO check for correct number of columns
 
 
 def test_robust_normalise_non_default_cols():
-    pass
+    out = normalise.robust_normalise(non_default_df, metadata_prefix="meta",
+                                     compound="meta_cmpd", plate_id="meta_plate")
+    assert isinstance(out, pd.DataFrame)
+    assert out.shape[0] == non_default_df.shape[0]
