d56be3917216d8faad7a755b1396ed74f1409359
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-03-27 10:27:01 +0100

    add missing module docstring

--- morar/dataframe.py
+++ morar/dataframe.py
@@ -6,7 +6,7 @@ import pandas as pd
 from morar import utils
 from morar import stats
 from morar import normalise
-
+from sklearn.decomposition import PCA
 
 class DataFrame(pd.DataFrame):
 
@@ -19,24 +19,24 @@ class DataFrame(pd.DataFrame):
     def __init__(self, data):
         pd.DataFrame.__init__(self, data)
 
-
+    @property
     def get_featuredata(self, **kwargs):
         """return featuredata"""
         featuredata_cols = utils.get_featuredata(self, **kwargs)
         return self[featuredata_cols]
 
-
+    @property
     def get_featurecols(self, **kwargs):
         """return of list feature data column names"""
         return utils.get_featuredata(self, **kwargs)
 
-
+    @property
     def get_metadata(self, **kwargs):
         """return metadata"""
         metadata_cols = utils.get_metadata(self, **kwargs)
         return self[metadata_cols]
 
-
+    @property
     def get_metacols(self, **kwargs):
         """return list of metadata column names"""
         return utils.get_metadata(self, **kwargs)
@@ -49,4 +49,16 @@ class DataFrame(pd.DataFrame):
 
     def normalise(self, **kwargs):
         return normalise.normalise(self, **kwargs)
+
+
+    def pca(self, **kwargs):
+        """return principal components dataframe and explained variance"""
+        pca = PCA(**kwargs)
+        featuredata = self.get_featuredata(**kwargs)
+        metadata = self.get_metadata(**kwargs)
+        pca_out = pca.fit_transform(featuredata)
+        pc_cols = ["PC" + str(i) for i in range(1, pca_out.shape[1]+1)]
+        only_pc = pd.DataFrame(pca_out, columns=pc_cols, index=metadata.index)
+        pca_df = pd.concat([only_pc, metadata], axis=1)
+        return [pca_df, pca.explained_variance_]
 
