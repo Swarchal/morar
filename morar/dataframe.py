4f2f695c6cc118700fceb6b7621c2a0d13bea057
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-04-12 10:40:09 +0100

    add morar.dataframe.DataFrame methods, PCA etc (fixes #14)

--- morar/dataframe.py
+++ morar/dataframe.py
@@ -20,24 +20,24 @@ class DataFrame(pd.DataFrame):
         pd.DataFrame.__init__(self, data)
 
     @property
-    def get_featuredata(self, **kwargs):
+    def featuredata(self, **kwargs):
         """return featuredata"""
         featuredata_cols = utils.get_featuredata(self, **kwargs)
         return self[featuredata_cols]
 
     @property
-    def get_featurecols(self, **kwargs):
+    def featurecols(self, **kwargs):
         """return of list feature data column names"""
         return utils.get_featuredata(self, **kwargs)
 
     @property
-    def get_metadata(self, **kwargs):
+    def metadata(self, **kwargs):
         """return metadata"""
         metadata_cols = utils.get_metadata(self, **kwargs)
         return self[metadata_cols]
 
     @property
-    def get_metacols(self, **kwargs):
+    def metacols(self, **kwargs):
         """return list of metadata column names"""
         return utils.get_metadata(self, **kwargs)
 
@@ -48,17 +48,52 @@ class DataFrame(pd.DataFrame):
 
 
     def normalise(self, **kwargs):
+        """normalise data via morar.normalise.normalise"""
         return normalise.normalise(self, **kwargs)
 
 
-    def pca(self, **kwargs):
-        """return principal components dataframe and explained variance"""
+    def query(self, string, **kwargs):
+        """pass query as in pd.DataFrame.query(string)"""
+        pd_data = pd.DataFrame(self)
+        result = pd_data.query(string, **kwargs)
+        return DataFrame(result)
+
+
+    def merge(self, right, **kwargs):
+        """merge via pandas.DataFrame.merge"""
+        pd_data = pd.DataFrame(self)
+        result = pd_data.merge(right, *kwargs)
+        return DataFrame(result)
+
+
+    def pca(self, var=True, **kwargs):
+        """
+        return principal components morar.Dataframe and explained variance
+
+        Parameters:
+        -----------
+        var : bool (default=True)
+            whether or not to return the explained variance of the components
+
+        Returns:
+        ---------
+        morar.DataFrame with calculated principal components and metadata
+        If var=True (default), also returns a list, where the second element is
+        the exaplained variance of the principal components as calculated by
+        `sklearn.decomposition.PCA.explained_variance_`
+        """
         pca = PCA(**kwargs)
         featuredata = self.get_featuredata(**kwargs)
         metadata = self.get_metadata(**kwargs)
         pca_out = pca.fit_transform(featuredata)
         pc_cols = ["PC" + str(i) for i in range(1, pca_out.shape[1]+1)]
         only_pc = pd.DataFrame(pca_out, columns=pc_cols, index=metadata.index)
-        pca_df = pd.concat([only_pc, metadata], axis=1)
-        return [pca_df, pca.explained_variance_]
+        pca_df = DataFrame(pd.concat([only_pc, metadata], axis=1))
+        if var is True:
+            return [pca_df, pca.explained_variance_]
+        elif var is False:
+            return pca_df
+        else:
+            msg = "expected bool for variance, received {}".format(type(var))
+            raise ValueError(msg)
 
